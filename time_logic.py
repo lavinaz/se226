f0acd223e56383f8eed55f011bda1c4a64e6ed99
Naz <nazgnyy@gmail.com>
2026-02-27 17:07:04 +0300

    Update time_logic.py
    my submision method was wrong so i fixed it

--- time_logic.py
+++ time_logic.py
@@ -1,10 +1,6 @@
-secnum=int(input("enter alarge integer representing a total number of seconds"))
+secnum = int(input("Enter a large integer representing total seconds: "))
 
+hours = secnum // 3600
 minutes = (secnum % 3600) // 60
-hours=minutes//60
 seconds = secnum % 60
 print(f"{secnum} seconds are {hours} hours, {minutes} minutes, {seconds} seconds.")
-
-
-
-
